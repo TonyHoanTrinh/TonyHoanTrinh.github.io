6093d7a6549c7ccc20ce8a9486334fb60c636266
Tony Trinh <anthonytrinh@hotmail.ca>
2019-12-31 12:29:48 -0500

    Updates

---
+++ precache-manifest.82e612e7678f9d58ae4a4f1114cce081.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42427d7c66a29f9b8682a940d658819c",
+    "url": "/p/index.html"
+  },
+  {
+    "revision": "551fae2d22ade4e9e589",
+    "url": "/p/static/css/main.f5283f9e.chunk.css"
+  },
+  {
+    "revision": "f9afd5ea8365935941f4",
+    "url": "/p/static/js/2.e04c03b4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/p/static/js/2.e04c03b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "551fae2d22ade4e9e589",
+    "url": "/p/static/js/main.10ca3873.chunk.js"
+  },
+  {
+    "revision": "8d05b037c3364179e772",
+    "url": "/p/static/js/runtime-main.624d9c9e.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/p/static/media/GithubLogo.76d344f0.svg"
+  },
+  {
+    "revision": "8220d48085242e88ff8450680fdd8f7e",
+    "url": "/p/static/media/LinkedInLogo.8220d480.svg"
+  },
+  {
+    "revision": "a47d80115093454e808a375f376d9ace",
+    "url": "/p/static/media/MainSquare.a47d8011.jpg"
+  },
+  {
+    "revision": "89ca82ccdfc4697c2bee09698cdf5a1a",
+    "url": "/p/static/media/Resume.89ca82cc.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6adbdf1ebbcca256bf676e9edd2b9dd6.js"
+  "/p/precache-manifest.82e612e7678f9d58ae4a4f1114cce081.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/p/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
